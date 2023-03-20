3eb51c564b83a20b8e633aae273fd284ce8bc6bd
Péter Ekler <peter.ekler@aut.bme.hu>
2023-03-20 13:06:09 +0100

    Class 12 demo
    TodoApplication main features (add, delete, checkbox, summary screen)

--- TodoDemo/app/src/main/java/hu/ait/tododemo/MainActivity.kt
+++ TodoDemo/app/src/main/java/hu/ait/tododemo/MainActivity.kt
@@ -10,7 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import hu.ait.tododemo.ui.screen.TodoListScreen
+import hu.ait.tododemo.ui.screen.TodoSummaryScreen
 import hu.ait.tododemo.ui.theme.TodoDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,8 +30,36 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TodoListScreen()
+                    TodoAppNavHost()
                 }
+            }
+        }
+    }
+}
+
+@Composable
+fun TodoAppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = "todolist"
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable("todolist") { TodoListScreen(navController = navController)}
+
+        composable("summary/{numalltodo}/{numimportant}",
+            arguments = listOf(
+                navArgument("numalltodo"){type = NavType.IntType},
+                navArgument("numimportant"){type = NavType.IntType}
+            )
+        ) {
+            val numalltodo = it.arguments?.getInt("numalltodo")
+            val numimportant = it.arguments?.getInt("numimportant")
+            if (numalltodo != null && numimportant != null) {
+                TodoSummaryScreen(numalltodo = numalltodo, numimportanttodo = numimportant)
             }
         }
     }
--- TodoDemo/app/src/main/java/hu/ait/tododemo/ui/screen/TodoListScreen.kt
+++ TodoDemo/app/src/main/java/hu/ait/tododemo/ui/screen/TodoListScreen.kt
@@ -1,12 +1,192 @@
 package hu.ait.tododemo.ui.screen
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.*
+import androidx.lifecycle.viewmodel.compose.viewModel
+import hu.ait.tododemo.data.TodoItem
+import hu.ait.tododemo.data.TodoPriority
+import java.util.*
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material3.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import hu.ait.tododemo.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TodoListScreen() {
+fun TodoListScreen(
+    todoListViewModel: TodoListViewModel = viewModel(),
+    navController: NavController
+) {
+    Column(
+        modifier = Modifier.padding(10.dp)
+    ) {
+        TopAppBar(
+            title = {
+                Text("AIT Todo")
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor =
+                MaterialTheme.colorScheme.secondaryContainer
+            ),
+            actions = {
+                IconButton(onClick = {
+                    navController.navigate(
+                        "summary/${todoListViewModel.getAllTodoNum()}/${todoListViewModel.getImportantTodoNum()}"
+                    )
+                }) {
+                    Icon(Icons.Filled.Info, null)
+                }
+            })
+
+
+        AddNewTodoForm()
+
+        LazyColumn() {
+            items(todoListViewModel.getAllToDoList()) {
+                TodoCard(todoItem = it,
+                    onTodoCheckChange = { checked ->
+                        todoListViewModel.changeTodoState(it, checked)
+                    },
+                    onRemoveItem = {
+                        todoListViewModel.removeTodoItem(it)
+                    }
+                )
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddNewTodoForm(
+    todoListViewModel: TodoListViewModel = viewModel()
+) {
+    var newTodoTitle by remember { mutableStateOf("") }
+    var newTodoDesc by remember { mutableStateOf("") }
+    var newTodoPriority by remember { mutableStateOf(false) }
+
     Column() {
-        Text(text = "Empty list")
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedTextField(value = newTodoTitle,
+                modifier = Modifier.weight(1f),
+                label = { Text(text = "Todo title") },
+                onValueChange = {
+                    newTodoTitle = it
+                }
+            )
+            OutlinedTextField(value = newTodoDesc,
+                modifier = Modifier.weight(1f),
+                label = { Text(text = "Description") },
+                onValueChange = {
+                    newTodoDesc = it
+                }
+            )
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(checked = newTodoPriority, onCheckedChange = {
+                newTodoPriority = it
+            })
+            Text(text = "Important")
+        }
+
+        Button(onClick = {
+            todoListViewModel.addTodoList(
+                TodoItem(
+                    UUID.randomUUID().toString(),
+                    newTodoTitle,
+                    newTodoDesc,
+                    Date(System.currentTimeMillis()).toString(),
+                    if (newTodoPriority) TodoPriority.HIGH else TodoPriority.NORMAL,
+                    false
+                )
+            )
+        }) {
+            Text(text = "Add")
+        }
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TodoCard(
+    todoItem: TodoItem,
+    onTodoCheckChange: (Boolean) -> Unit = {},
+    onRemoveItem: () -> Unit = {}
+) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        ),
+        shape = RoundedCornerShape(20.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        ),
+        modifier = Modifier.padding(5.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(20.dp)
+        ) {
+
+            Image(
+                painter = painterResource(id = todoItem.priority.getIcon()),
+                contentDescription = "Priority",
+                modifier = Modifier
+                    .size(40.dp)
+                    .padding(end = 10.dp)
+            )
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+            ) {
+                Text(
+                    text = todoItem.title,
+                    textDecoration =
+                    if (todoItem.isDone) {
+                        TextDecoration.LineThrough
+                    } else {
+                        TextDecoration.None
+                    }
+                )
+            }
+
+            Row {
+                Checkbox(
+                    checked = todoItem.isDone,
+                    onCheckedChange = {
+                        onTodoCheckChange(it)
+                    },
+                )
+                Icon(
+                    imageVector = Icons.Filled.Delete,
+                    contentDescription = "Delete",
+                    modifier = Modifier.clickable {
+                        onRemoveItem()
+                    },
+                    tint = Color.Red
+                )
+            }
+        }
     }
 }
